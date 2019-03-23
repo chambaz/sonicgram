07f8f5375e65b9b58aceb9f9a6a8821ec9d6d5e1
Adam Chambers <adammchambers@gmail.com>
2019-03-23 15:47:57 -0400

    📝  Documentation and code cleanup

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import getUserMedia from 'getusermedia'
 import Sentiment from 'sentiment'
 import styled from '@emotion/styled'
 
+// styled components
 const Page = styled.div({
   display: 'flex',
   alignItems: 'center',
@@ -53,17 +54,21 @@ function Index() {
 
   const sentiment = new Sentiment()
 
+  // DOM node references
+  // TODO: useState not playing nicely with browser APIs
   const recordBtn = useRef()
   const wave = useRef()
   const result = useRef()
   const score = useRef()
 
+  // when component first mounts load and configure browser APIs
   useEffect(() => {
     recordRtc = require('recordrtc')
     waveSurfer = require('wavesurfer.js')
     canvasToImage = require('canvas-to-image')
     vsr = require('voice-speech-recognition')
 
+    // initialize wave surfer
     waver = waveSurfer.create({
       container: wave.current,
       waveColor: 'violet',
@@ -72,9 +77,11 @@ function Index() {
       height: 250
     })
 
+    // initialize WebSpeech API library
     recognizer = vsr.voiceSpeechRecognition()
   })
 
+  // user kicks off recording
   function start() {
     getUserMedia(
       {
@@ -82,19 +89,25 @@ function Index() {
         audio: true
       },
       (err, stream) => {
+        // usermedia not supported or user declined
         if (err) {
           console.log('User media failed')
           console.log(err)
         } else {
           recordBtn.current.innerHTML = 'Recording...'
+
+          // consifgure usermedia recording library
           recorder = recordRtc(stream, {
             type: 'audio',
             recorderType: recordRtc.StereoAudioRecorder
           })
 
+          // start recording audio and recognizing speech
           recorder.startRecording()
           recognizer.startRecognition()
 
+          // when speech recognition is complete
+          // pipe through sentiment analysis and print to screen
           recognizer.addEventListener('end', () => {
             const sentimentAnalysis = sentiment.analyze(
               recognizer.finalRecognizing
@@ -110,20 +123,26 @@ function Index() {
     )
   }
 
+  // user stops recording
   function stop() {
     recorder.stopRecording(blob => {
+      // stop speech recognition
       recognizer.stopRecognition()
       recordBtn.current.innerHTML = 'Record'
+
+      // print waveform
       recorder.getDataURL(data => {
         drawWaveForm(data)
       })
     })
   }
 
+  // play back recorded audio
   function play() {
     waver.play()
   }
 
+  // download waveform as png
   function download() {
     wave.current.querySelector('canvas').id = 'canvas'
     canvasToImage('canvas', {
@@ -133,6 +152,7 @@ function Index() {
     })
   }
 
+  // clear waveform and start again
   function drawWaveForm(data) {
     waver.empty()
     waver.load(data)
